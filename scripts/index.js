8ef865072e189ee2dd92d75ed970ad3eeb61b9ff
hasankarim18 <hallysust@gmail.com>
2023-07-04 14:51:35 +0600

    showing cart

--- scripts/index.js
+++ scripts/index.js
@@ -26,7 +26,7 @@ fetch("http://localhost:5000/products")
     <div  class="border  rounded-xl w-full bg-base-100 card">
         <figure><img class="h-60 w-full" src="${item.image}" /></figure>
         <div class="card-body">
-            <span class="product_id" > ${item.product_id} </span> 
+           <div>Product Id:  <span class="product_id" > ${item.product_id} </span>  </div>
             <h2 class="card-title">${item.product_name}</h2>
             <p class="font-bold text-rose-400"> Price: $<span class="price">${item.price}</span>/-</p>
             <div class="card-actions justify-end">
@@ -36,8 +36,7 @@ fetch("http://localhost:5000/products")
     </div>           
   `;
     productContainer.appendChild(div);
-    });
-  
+    });  
   })
   .then(()=> {
     const buyNowButtons = document.querySelectorAll(".buy-now");
@@ -49,9 +48,7 @@ fetch("http://localhost:5000/products")
   .catch((err) => {
     console.log(err);
   });
-
 }
-
 
 function handleBuyNow(event) {
   const button = event.target;
@@ -60,11 +57,52 @@ function handleBuyNow(event) {
   const priceFloat = parseFloat(price.toFixed(2));
   const productId = parent.querySelector('.product_id').innerText
 
-  const cart = localStorage.getItem('js_cart')
-  
+  let cartData = localStorage.getItem('js_cart')
 
+  if(cartData){
+    cartData = JSON.parse(cartData)
+  }else {
+    cartData = []
+  }
+
+
+  // checking if product exists 
+   const existingProductIndex = cartData.findIndex(
+     (item) => item.product_id === productId
+   );
+   if (existingProductIndex !== -1) {
+     // If the product is already in the cart, increment the quantity
+     console.log('exists');
+     cartData[existingProductIndex].quantity++;
+   } else {
+     // If the product is not in the cart, add it to the cart with a quantity of 1
+     console.log('new product');
+     cartData.push({
+       product_id: productId,
+       quantity: 1,
+       price:priceFloat
+     });
+   }
+
+   localStorage.setItem("js_cart", JSON.stringify(cartData));
+
+   showTotalItem();
+
+   // Show a confirmation message
+ //  alert("Product added to cart successfully!");
 
 };
+
+
+const showTotalItem = ()=> {
+  const cartData = JSON.parse(localStorage.getItem('js_cart'))
+  const totalNumberOfProducts = cartData.reduce((total,cart )=> total + cart.quantity   , 0)
+  console.log(totalNumberOfProducts);
+  document.getElementById("totalItem").innerHTML = totalNumberOfProducts;
+
+}
+
+showTotalItem()
 
 
 
